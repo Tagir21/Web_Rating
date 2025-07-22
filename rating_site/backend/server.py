00b00b4a5ae09fc8ba6054dad70110247fbc587d
nakloroo <nakloroo@student.21-school.ru>
2025-07-22 15:19:46 +0300

    add_cors_settings

--- rating_site/backend/server.py
+++ rating_site/backend/server.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db_conn import get_all_grades
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8001"],
+    allow_methods=["GET"]
+)
 
 @app.get('/get_all_akademy_grades')
 async def get_all_akademy_grades():
