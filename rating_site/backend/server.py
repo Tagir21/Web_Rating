6ccd19814a8b136adac4383888b5bb3c1be23936
nakloroo <nakloroo@student.21-school.ru>
2025-07-22 15:35:54 +0300

    testing str

--- rating_site/backend/server.py
+++ rating_site/backend/server.py
@@ -14,5 +14,5 @@ app.add_middleware(
 @app.get('/get_all_akademy_grades')
 async def get_all_akademy_grades():
     all_akademy_grades = await get_all_grades()
-
+    print("Я работаю, всё ок")
     return {'grades': all_akademy_grades}
