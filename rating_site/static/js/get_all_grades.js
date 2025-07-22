2476ea258d5e656126ba3c450528330c586fe1b6
nakloroo <nakloroo@student.21-school.ru>
2025-07-22 03:38:15 +0300

    JS script для прокидывания данных

---
+++ rating_site/static/js/get_all_grades.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const response = await fetch("localhost:8000/___");
+        const data = await response.json();
+
+        const table_body = document.getElementById('grades_table_body');
+
+        if (data.akademy_grades && data.akademy_grades.length > 0) { // Пока что только для академика
+            let html = '';
+            data.grades.forEach((akademy_grade, index) => {
+                html += '
+                    <tr>
+                        <th scope="row">${index + 1}></th>
+                        <td>{akademy_grade.user_id}</td>
+                        <td>{akademy_grade.grade}</td>
+                    </tr>
+                ';
+            });
+            table_body.innerHTML = html;
+        } else {
+            table_body.innerHTML = '
+                <tr>
+                    <td cosplan="3" class="text-center">Нет данных о студентах</td>
+                </tr>
+            ';
+        }
+    } catch (error) {
+        console.error("Ошибка загрузки:", error);
+        document.getElementById("grades_table_body").innerHTML = '
+            <tr>
+                <td cosplan="3" class="text-center">Ошибка загрузки данных</td>
+            </tr>
+        ';
+    }
+}
